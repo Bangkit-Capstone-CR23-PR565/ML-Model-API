7974659c70870751a223d9998a93b7305d640584
fathanm10 <fathan.muhammad01@ui.ac.id>
2023-06-08 17:45:19 +0700

    add new endpoint for filtering rank and relevancy

--- main.py
+++ main.py
@@ -17,20 +17,25 @@ async def index():
 async def items(user_id: int, limit: Union[int, None] = None):
     return model_handler.retrieval_model(user_id)[:limit]
 
-@app.get("/event/top-recommendations/{user_id}")
+@app.get("/event/top-ranking-predictions/{user_id}")
 async def rank(user_id: int, limit: Union[int, None] = None):
     return model_handler.ranking_model(user_id)[:limit]
+
+@app.get("/event/top-recommendations/{user_id}")
+async def recommend(user_id: int, limit: Union[int, None] = None):
+    retrieval = model_handler.retrieval_model(user_id)
+    retrieval_ids = set()
+    for i in retrieval:
+        if len(retrieval_ids) >= limit:
+            break
+        retrieval_ids.add(i['id'])
+    ranking = model_handler.ranking_model(user_id)
+    ranking_filtered = [i for i in ranking if i['id'] in retrieval_ids]
+    return ranking_filtered
 
 @app.get("/event/search/{query}")
 async def search(query: str, limit: Union[int, None] = None):
     return model_handler.tags_search_model(query, top_n=limit)
-
-# Current: events from database, ratings from csv
-@app.get("/update-data")
-async def update_data():
-    scraper.fetch_events()
-    scraper.fetch_ratings()
-    return "Data updated"
 
 @app.get("/retrain")
 async def retrain():
@@ -40,8 +45,15 @@ async def retrain():
 @app.on_event('startup')
 @repeat_every(seconds=60*60*12, wait_first=True)   # schedule task every half day
 async def scheduled_task():
-    print(update_data())
+    # print(update_data())
     print(model_trainer.retrain_all())
+
+# Current: events from database, ratings from csv
+# @app.get("/update-data")
+# async def update_data():
+#     scraper.fetch_events()
+#     scraper.fetch_ratings()
+#     return "Data updated"
 
 # def create_db():
 #     Event.__table__.create(engine)
